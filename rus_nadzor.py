d5ae2decbc1210f16e94e1e5c9b9e7fd0e22e10d
balu83vs <106230624+balu83vs@users.noreply.github.com>
2022-05-29 22:36:36 +0300

    29/05

--- rus_nadzor.py
+++ rus_nadzor.py
@@ -3,8 +3,9 @@ word = input() + ' запретил букву'
 for i in range(ord('Я'), ord('я') + 1):
     mylist = [c for c in word if c != chr(i)]
     word = ''.join(mylist)
+    
     if chr(i + 1) not in word:
         continue   
     else:    
-        print(word + ' ' + chr(i + 1)) 
+        print(word.lstrip().replace('  ', ' ') + ' ' + chr(i + 1)) 
     mylist = []
